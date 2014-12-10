f62ffdb4e1b1a50585be4447ad69db00b49008ca
Taylor McKinnon <tokintmac@gmail.com>
2014-12-09 21:51:59 -0600

    lets try this?

--- host/host.go
+++ host/host.go
@@ -31,9 +31,9 @@ type HostManager struct {
   hosts = map[string]*Host{}
 }
 
-func (h *HostManager) register(name string) host Host {
+func (h *HostManager) register(name string) (host Host) {
   hostId = uuid.New()
-  host := Host{id: hostId, name: name}
+  host = Host{id: hostId, name: name}
   h.hosts[hostId] = &host
   fmt.Printl(hostId)
   return
